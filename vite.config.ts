3707645d9e2a21688d1e09ac646062cb51617151
reinking23-blip <reinking23@gmail.com>
2026-01-12 14:47:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // 将 'reinking23-blip' 替换为你仓库的实际名称
-  base: '/reinking23-blip/', 
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
+  base: '/Enterprise-WeChat-Transformation-Plan/', 
   plugins: [react()],
   resolve: {
     alias: {
